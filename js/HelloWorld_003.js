d35d2fc9bd36935ee6cbb5b009c890812a4c5256
FennecFoxSW <FennecFoxSW@protonmail.com>
2021-07-12 01:23:40 +0900

    FennecFoxSW : JavaScript

--- js/HelloWorld_003.js
+++ js/HelloWorld_003.js
@@ -2,5 +2,7 @@ const hello_world_express = require('express');
 const hello_world_app = hello_world_express();
 
 hello_world_app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!');
 });
+
+hello_world_app.listen(3000);
